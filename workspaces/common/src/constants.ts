8892e526815c959e123fa1497aacd6f1250f7712
Akuukis <akuukis@kalvis.lv>
2019-10-21 15:45:52 +0300

    chore(*): move db_host to env

--- workspaces/common/src/constants.ts
+++ workspaces/common/src/constants.ts
@@ -1,5 +1,4 @@
 //            semver = 000.000.002
 export const VERSION =           2
-export const DB_URL = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@`
-                    + `spaceengineersblueprintspraised-nfrur.mongodb.net/test`
+export const DB_URL = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@${process.env.db_host}`
 export const DB_NAME = 'default'
