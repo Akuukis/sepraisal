1b6c9792b0f091dc88c949145cc46b2a4c8d4e10
Akuukis <akuukis@kalvis.lv>
2019-12-08 16:09:31 +0200

    fix(cli): simpler url instead of user:pass@host

--- workspaces/common/src/constants.ts
+++ workspaces/common/src/constants.ts
@@ -1,4 +1,4 @@
 //            semver = 000.000.002
 export const VERSION =           2
-export const DB_URL = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@${process.env.db_host}`
+export const DB_URL = String(process.env.db_url)
 export const DB_NAME = 'default'
