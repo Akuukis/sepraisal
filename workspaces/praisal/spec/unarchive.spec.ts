596044f33cea6511b0fe6c0ffaf90fb71e626d98
Akuukis <akuukis@kalvis.lv>
2019-12-14 13:50:31 +0200

    test(praisal): snapshot praisals

--- workspaces/praisal/spec/unarchive.spec.ts
+++ workspaces/praisal/spec/unarchive.spec.ts
@@ -14,13 +14,21 @@ beforeEach(async () => {
     sepraisal = await newPraisalManager()
 })
 
+const testBlueprint = (title: string, archive: string) => {
+    test(`should succeed to praise random small steam blueprint (${title})`, async () => {
+        const xml = await parseSteamArchive(createReadStream(join(FIXTURES_DIR, archive)))
+        const praisal = await sepraisal.praiseXml(xml)
+        expect(praisal.toBlueprintSbc(0)).toMatchSnapshot({
+            integrityPlanes: {
+                front: expect.any(Array),
+                side: expect.any(Array),
+                top: expect.any(Array),
+            },
+        })
+    })
+}
+
 describe('PraisalManager.praiseXml', () => {
-    test('should succeed to praise random small steam blueprint (Wyvern - Atmospheric Survival Ship)', async () => {
-        const xml = await parseSteamArchive(createReadStream(join(FIXTURES_DIR, '659278800.1.zip')))
-        const praisal = await sepraisal.praiseXml(xml)
-    })
-    test('should succeed to praise fast big steam blueprint (O.S.C. Aldebaran-Class Heavy Cruiser)', async () => {
-        const xml = await parseSteamArchive(createReadStream(join(FIXTURES_DIR, '383985794.2.zip')))
-        const praisal = await sepraisal.praiseXml(xml)
-    })
+    testBlueprint('Wyvern - Atmospheric Survival Ship)', '659278800.1.zip')
+    testBlueprint('O.S.C. Aldebaran-Class Heavy Cruiser)', '383985794.2.zip')
 })
