1fc2137e02e3a7438f8a7a19ceb3ecc15d69d8c1
Akuukis <akuukis@kalvis.lv>
2020-01-11 14:12:54 +0200

    fix(cli): typo jpg -> jpeg

--- workspaces/cli/src/routines/3-thumbnail.ts
+++ workspaces/cli/src/routines/3-thumbnail.ts
@@ -46,10 +46,10 @@ export const thumbConvert = async (idPair: string) => {
         await execAsync(`curl -s '${link}' -o '${safeFilename}'`)
     }
 
-    const format = (await execAsync(`identify -format "%m\n" ${safeFilename} | head -n1`)).trim() as 'PNG' | 'JPG' | 'GIF'
+    const format = (await execAsync(`identify -format "%m\n" ${safeFilename} | head -n1`)).trim() as 'PNG' | 'JPEG' | 'GIF'
     switch(format) {
         case('PNG'):
-        case('JPG'): {
+        case('JPEG'): {
             return execAsyncBuffer(`cat ${safeFilename} | cwebp -preset default -resize 268 151 -pass 10 -mt -af -size ${QUALITY} -quiet -o - -- -`)
         }
         case('GIF'): {
