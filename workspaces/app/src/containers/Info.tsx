c1c91c26cb5d03fec59eacd29a7f90279ba05f5b
Akuukis <akuukis@kalvis.lv>
2020-01-19 11:34:47 +0200

    fix(app): better Credits

--- workspaces/app/src/containers/Info.tsx
+++ workspaces/app/src/containers/Info.tsx
@@ -38,29 +38,28 @@ export default hot(createSmartFC(styles)<IProps>(({children, classes, theme, ...
                 <Paper className={classes.content}>
                     <Typography variant='h4' gutterBottom>Credits</Typography>
                     <Divider />
-                    <Typography variant='h5'>Author</Typography>
-                    <Typography component='div' paragraph>
+                    <Typography variant='h5'>Maintainers</Typography>
+                    <Typography paragraph component='div'>
                         <ul>
-                            <li>Akuukis (contact me
-                                on: <a href='https://steamcommunity.com/id/akuukis'>Steam</a>
-                                , <a href='https://github.com/Akuukis'>Github</a>
-                                , <a href='https://gitlab.com/Akuukis'>GitLab</a>
-                                , or Discord: Akuukis#6154)
+                            <li>
+                                <strong>Akuukis</strong> -
+                                contact on: <a href='//github.com/Akuukis'>Github</a>, <a href='//gitlab.com/Akuukis'>GitLab</a>, or Discord: Akuukis#6154.
                             </li>
-                        </ul>
-                    </Typography>
-                    <Typography variant='h5'>Contributors</Typography>
-                    <Typography component='div' paragraph>
-                        <ul>
-                            <li>kainz (<a href='https://github.com/Akuukis/spaceengineers-praisal/pull/3'>PR#3</a>)</li>
-                            <li>Aleksis Zalitis (<a href='https://gitlab.com/akuukis/sepraisal/merge_requests/1'>MR!1</a>)</li>
+                            <li><em>... looking for maintainers (if interested, DM Akuukis) ...</em></li>
                         </ul>
                     </Typography>
                     <Typography variant='h5'>Keen Software House</Typography>
                     <Typography paragraph>
                         <a href='https://www.keenswh.com/'>You</a> are great! Thanks for making <a href='https://www.spaceengineersgame.com/'>Space Engineers</a> :)
                     </Typography>
-                    <Typography paragraph>Game assets are used with Keen Software House permission in personal communication.</Typography>
+                    <Typography variant='h5'>Legal</Typography>
+                    <Typography paragraph>
+                        This website is open-source (<a href='//choosealicense.com/licenses/gpl-3.0/'>GPLv3</a>).
+                        You can find source code at <a href='//gitlab.com/akuukis/sepraisal'>Gitlab</a>.
+                    </Typography>
+                    <Typography paragraph>
+                        Game assets are used with Keen Software House permission in personal communication with Akuukis.
+                    </Typography>
                 </Paper>
             </Grid>
         </Grid>
