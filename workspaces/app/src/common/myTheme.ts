265ce63787a855632d8edddf0561e4962547edd1
Akuukis <akuukis@kalvis.lv>
2020-04-19 23:18:57 +0300

    feat(app): better colors at theme

--- workspaces/app/src/common/myTheme.ts
+++ workspaces/app/src/common/myTheme.ts
@@ -4,12 +4,13 @@ import { green, purple, red } from '@material-ui/core/colors'
 // Official Space Engineers colors (https://blog.marekrosa.org/2017/09/my-vision-for-visual-style-of-space.html)
 export const SE_COLORS = {
     black: '#111111',
-    blue: '#1767ae',
-    green: '#417e3b',
     grey: '#b6bab9',
     grey_dark: '#686868',
-    red: '#c01118',
     white: '#efefef',
+
+    blue: '#1767ae',
+    green: '#417e3b',
+    red: '#c01118',
     yellow: '#f5bf2b',
 }
 
@@ -17,19 +18,20 @@ export const SE_COLORS = {
 export interface IMyTheme extends Theme {
 }
 
+// www.color-hex.com/color
 export const MY_LIGHT_THEME: IMyTheme = createMuiTheme({
     palette: {
-        error: red,
-        grey: {
-            500: SE_COLORS.grey,
-        },
         primary: {
-            ...purple,
-            500: SE_COLORS.blue,
+            ...red,
+            main: SE_COLORS.blue,
+            dark:'#12528b',  // 2 darker.
+            light: '#5c94c6',  // 3 lighter.
         },
         secondary: {
             ...green,
-            A400: '#00e677',
+            main: SE_COLORS.yellow,
+            dark: '#c49822',  // 2 darker.
+            light: '#fbe5aa',  // 6 lighter.
         },
     },
     spacing: 4,
