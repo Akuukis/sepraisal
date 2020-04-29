1e1a30e6a1370cb6cb26cba31d9dd53d1d84a577
Akuukis <akuukis@kalvis.lv>
2020-04-29 15:09:30 +0300

    fix(app): nicier upload

--- workspaces/app/src/containers/Compare/PanelUploads.tsx
+++ workspaces/app/src/containers/Compare/PanelUploads.tsx
@@ -1,18 +1,14 @@
-import { noop } from '@sepraisal/common'
 import clsx from 'clsx'
-import { runInAction } from 'mobx'
-import * as Pako from 'pako'
 import * as React from 'react'
-import { useDropzone } from 'react-dropzone'
 import { hot } from 'react-hot-loader/root'
 
-import { ExpansionPanelProps, Link, List, ListItem, Typography } from '@material-ui/core'
+import { ExpansionPanelProps, List } from '@material-ui/core'
 
-import { createSmartFC, createStyles, DUD_URL as NOOP_URL, IMyTheme } from '../../common'
+import { createSmartFC, createStyles, IMyTheme } from '../../common'
 import MyExpansionPanel from '../../components/MyExpansionPanel'
 import { CONTEXT } from '../../stores'
-import SelectorDnDOverlay from './DnDOverlay'
 import PanelUploadsRow from './PanelUploadsRow'
+import PanelUploadsUpload from './PanelUploadsUpload'
 
 const styles = (theme: IMyTheme) => createStyles({
     root: {
@@ -34,59 +30,6 @@ interface IProps extends ExpansionPanelProps {
 export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes, theme, ...props}) => {
     const { className, ...otherProps } = props
     const blueprintStore = React.useContext(CONTEXT.BLUEPRINTS)
-    const piwikStore = React.useContext(CONTEXT.PIWIK)
-    const selectionStore = React.useContext(CONTEXT.SELECTION)
-    const praisalManager = React.useContext(CONTEXT.PRAISAL_MANAGER)
-
-    const onDrop = React.useCallback(async (acceptedFiles, rejectedFiles) => {
-        for(const file of acceptedFiles) {
-            try {
-                const reader = new FileReader()
-                const xml = await new Promise((resolve: (res: string) => void, reject) => {
-                    reader.onload = (event) => {
-                            // tslint:disable-next-line: no-any - TODO fix typings
-                            const {result}: any = event.target
-                            try {
-                                const out = Pako.inflate(result, {to: 'string'})
-                                resolve(out)
-                            } catch(error) {
-                                console.error(`inflate failed ${error}`)
-                                const out = result.toString('utf-8')
-                                resolve(out)
-                            }
-                    }
-                    reader.onabort = reject
-                    reader.onerror = reject
-                    reader.readAsText(file)
-                })
-
-                const title = blueprintStore.setUpload(await praisalManager.praiseXml(xml))
-                runInAction(() => {
-                    selectionStore.selected.push(title)
-                })
-                piwikStore.push([
-                    'trackEvent',
-                    'workshop',
-                    'upload-successful',
-                    title,
-                    undefined,
-                ])
-
-            } catch(err) {
-                piwikStore.push([
-                    'trackEvent',
-                    'workshop',
-                    'upload-failed',
-                    err.message,
-                    undefined,
-                ])
-
-            }
-        }
-    }, [])
-
-
-    const {getRootProps, getInputProps, isDragActive, open: browseFiles} = useDropzone({onDrop})
 
     return (
         <MyExpansionPanel
@@ -96,14 +39,8 @@ export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes
             classes={{details: classes.details, secondaryHeading: classes.secondaryHeading}}
             {...otherProps}
         >
-            <input {...getInputProps()} />
-            {isDragActive ? <SelectorDnDOverlay /> :  null}
             <List dense className={classes.list}>
-                <ListItem key='0' {...{...getRootProps(), onClick: noop}}>
-                    <Typography color='textSecondary' variant='body2' align='center'>
-                        "Drag and drop" or&nbsp;<Link href={NOOP_URL} variant='body1' onClick={browseFiles}>upload</Link>&nbsp; (sbc only).
-                    </Typography>
-                </ListItem>
+                <PanelUploadsUpload />
                 {[...blueprintStore.uploads].map<JSX.Element>(([key]) => (
                     <PanelUploadsRow
                         key={key}
---
+++ workspaces/app/src/containers/Compare/PanelUploadsUpload.tsx
@@ -0,0 +1,101 @@
+import { noop } from '@sepraisal/common'
+import { runInAction } from 'mobx'
+import * as Pako from 'pako'
+import * as React from 'react'
+import { useDropzone } from 'react-dropzone'
+import { hot } from 'react-hot-loader/root'
+
+import { Link, ListItem, Typography } from '@material-ui/core'
+
+import { createSmartFC, createStyles, DUD_URL as NOOP_URL, IMyTheme } from '../../common'
+import { CONTEXT } from '../../stores'
+import SelectorDnDOverlay from './DnDOverlay'
+
+const styles = (theme: IMyTheme) => createStyles({
+    root: {
+        borderStyle: `dashed`,
+        borderWidth: theme.spacing(1),
+        borderColor: `${theme.palette.text.secondary}B0`,
+        '&:hover': {
+            color: theme.palette.text.primary,
+            borderColor: theme.palette.text.secondary,
+        },
+        height: '72px',
+    },
+
+    text: {
+        width: '100%',
+    },
+})
+
+
+interface IProps {
+}
+
+
+export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes, theme, ...props}) => {
+    const blueprintStore = React.useContext(CONTEXT.BLUEPRINTS)
+    const piwikStore = React.useContext(CONTEXT.PIWIK)
+    const selectionStore = React.useContext(CONTEXT.SELECTION)
+    const praisalManager = React.useContext(CONTEXT.PRAISAL_MANAGER)
+
+    const onDrop = React.useCallback(async (acceptedFiles, rejectedFiles) => {
+        for(const file of acceptedFiles) {
+            try {
+                const reader = new FileReader()
+                const xml = await new Promise((resolve: (res: string) => void, reject) => {
+                    reader.onload = (event) => {
+                            // tslint:disable-next-line: no-any - TODO fix typings
+                            const {result}: any = event.target
+                            try {
+                                const out = Pako.inflate(result, {to: 'string'})
+                                resolve(out)
+                            } catch(error) {
+                                console.error(`inflate failed ${error}`)
+                                const out = result.toString('utf-8')
+                                resolve(out)
+                            }
+                    }
+                    reader.onabort = reject
+                    reader.onerror = reject
+                    reader.readAsText(file)
+                })
+
+                const title = blueprintStore.setUpload(await praisalManager.praiseXml(xml))
+                runInAction(() => {
+                    selectionStore.selected.push(title)
+                })
+                piwikStore.push([
+                    'trackEvent',
+                    'workshop',
+                    'upload-successful',
+                    title,
+                    undefined,
+                ])
+
+            } catch(err) {
+                piwikStore.push([
+                    'trackEvent',
+                    'workshop',
+                    'upload-failed',
+                    err.message,
+                    undefined,
+                ])
+
+            }
+        }
+    }, [])
+
+
+    const {getRootProps, getInputProps, isDragActive, open: browseFiles} = useDropzone({onDrop})
+
+    return (
+        <ListItem className={classes.root} {...{...getRootProps(), onClick: noop}}>
+            <input {...getInputProps()} />
+            {isDragActive ? <SelectorDnDOverlay /> :  null}
+            <Typography className={classes.text} variant='body2' align='center'>
+                Drop .sbc file here or&nbsp;<Link href={NOOP_URL} onClick={browseFiles}>click to upload</Link>.
+            </Typography>
+        </ListItem>
+    )
+})) /* ============================================================================================================= */
