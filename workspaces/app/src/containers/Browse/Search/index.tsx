455ed95fe0fe86724e724fdfef2c8d3050fe3d54
Akuukis <akuukis@kalvis.lv>
2020-05-13 19:09:25 +0300

    refactor(app): move search into a folder

---
+++ workspaces/app/src/containers/Browse/Search/index.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react'
+import { hot } from 'react-hot-loader/root'
+
+import { Grid, Paper, Typography } from '@material-ui/core'
+
+import { createSmartFC, createStyles, IMyTheme } from 'src/common'
+import { CONTEXT } from 'src/stores'
+
+import Search from './Search'
+import Sort from './Sort'
+
+const styles = (theme: IMyTheme) => createStyles({
+    root: {
+    },
+
+    input: {
+        backgroundColor: 'white',
+    },
+    autocomplete: {
+        minWidth: 360,
+        maxWidth: 720,
+        flexGrow: 1,
+        flexShrink: 1,
+    },
+    status: {
+        flexGrow: 0,
+        flexShrink: 0,
+        padding: theme.spacing(2),
+        color: theme.palette.primary.contrastText,
+        minWidth: 240,
+    },
+})
+
+
+interface IProps {
+}
+
+
+export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes, theme, ...props}) => {
+    const cardStore = React.useContext(CONTEXT.CARDS)
+
+    return (
+        <Grid
+            className={classes.root}
+            item
+            xs={12}
+
+            container
+            justify='center'
+            alignItems='center'
+        >
+            <Search />
+            <Paper>
+                <Sort />
+            </Paper>
+            <Typography className={classes.status} variant='subtitle1' align='center'>
+                {cardStore.count && `showing ${cardStore.cards.size} of ${cardStore.count} results`}
+            </Typography>
+        </Grid>
+    )
+})) /* ============================================================================================================= */
