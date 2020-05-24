a71ae627c5eab88b937a49e53213702b5e959a03
Akuukis <akuukis@kalvis.lv>
2020-05-24 18:00:51 +0300

    fix(app): copy comparison

--- workspaces/app/src/containers/Compare/Panel.tsx
+++ workspaces/app/src/containers/Compare/Panel.tsx
@@ -1,9 +1,11 @@
+import clipboard from 'clipboard-polyfill'
 import * as React from 'react'
 import { hot } from 'react-hot-loader/root'
 
-import { Divider, Grid, Link, Switch, Typography } from '@material-ui/core'
+import { Button, ClickAwayListener, Divider, Grid, Link, Switch, Tooltip, Typography } from '@material-ui/core'
 
 import { createSmartFC, createStyles, DUD_URL, formatDecimal, IMyTheme } from 'src/common'
+import IconCopy from 'src/components/icons/IconCopy'
 import { CONTEXT } from 'src/stores'
 
 import PanelFavorites from './PanelFavorites'
@@ -33,7 +35,10 @@ const styles = (theme: IMyTheme) => createStyles({
     },
     footer: {
         padding: theme.spacing(2),
-    }
+    },
+    button: {
+        margin: theme.spacing(2),
+    },
 })
 
 
@@ -44,10 +49,17 @@ interface IProps {
 export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes, theme, ...props}) => {
     const blueprintStore = React.useContext(CONTEXT.BLUEPRINTS)
     const selectionStore = React.useContext(CONTEXT.SELECTION)
+    const [open, setOpen] = React.useState(false)
 
     const panelClasses = {
         root: classes.subpanel,
         list: classes.list,
+    }
+
+    const copy = () => {
+        clipboard.writeText(document.location.href)
+        setOpen(true)
+        setTimeout(close, 6000)
     }
 
     const toggleNarrow = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -56,7 +68,30 @@ export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes
 
     return (
         <div className={classes.root}>
-            <Grid container justify='flex-end'>
+            <Grid container justify='space-between'>
+                <ClickAwayListener onClickAway={close}>
+                    <Tooltip
+                        PopperProps={{disablePortal: true}}
+                        placement='right'
+                        onClose={close}
+                        open={open}
+                        disableFocusListener
+                        disableHoverListener
+                        disableTouchListener
+                        title='Link to the comparison has been copied to your clipboard.'
+                    >
+                        <Button
+                            className={classes.button}
+                            variant='outlined'
+                            color='primary'
+                            size='small'
+                            onClick={copy}
+                        >
+                            <IconCopy />
+                            Copy
+                        </Button>
+                    </Tooltip>
+                </ClickAwayListener>
                 <Grid item className={classes.switchItem}>
                     <Switch checked={selectionStore.narrow} onChange={toggleNarrow} />
                     <Typography component='label' variant='body1'>narrow columns</Typography>
