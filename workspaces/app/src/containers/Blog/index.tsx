23bc15e9cc080c455751a1480649096829ed9eaa
Akuukis <akuukis@kalvis.lv>
2020-05-28 14:55:24 +0300

    fix(app): add test markdown article

--- workspaces/app/src/containers/Blog/index.tsx
+++ workspaces/app/src/containers/Blog/index.tsx
@@ -3,12 +3,19 @@ import { hot } from 'react-hot-loader/root'
 
 import { Grid } from '@material-ui/core'
 
-import { createSmartFC, createStyles, IMyTheme } from 'src/common'
+import { createSmartFC, createStyles, IMyTheme, useAsyncEffectOnce } from 'src/common'
+import Markdown from 'src/components/Markdown'
 import DefaultLayout from 'src/layouts/DefaultLayout'
+
+import postLink from '../../../static/test.md'
 
 const styles = (theme: IMyTheme) => createStyles({
     root: {
     },
+
+    paper: {
+        padding: theme.spacing(2),
+    }
 })
 
 
@@ -17,12 +24,21 @@ interface IProps {
 
 
 export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes, theme, ...props}) => {
+    const [text, setText] = React.useState('')
+
+    useAsyncEffectOnce(async () => {
+        const response = await fetch(postLink)
+        const markdown = await response.text()
+        setText(markdown)
+    })
 
     return (
         <DefaultLayout className={classes.root}>
             <Grid container spacing={2} justify='center'>
-                <Grid item xs={12} sm={10} md={8} lg={6}>
-                    Hello world
+                <Grid item>
+                    <Markdown>
+                        {text}
+                    </Markdown>
                 </Grid>
             </Grid>
         </DefaultLayout>
