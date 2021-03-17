f7537bdd2a7e06a08a0cf9d37d15c7c5dfbcdb67
Akuukis <akuukis@kalvis.lv>
2021-03-17 19:12:05 +0200

    fix(app): remove bad server alert

--- workspaces/app/src/containers/Browse/index.tsx
+++ workspaces/app/src/containers/Browse/index.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader/root'
 
-import { Snackbar } from '@material-ui/core'
-import { Alert } from '@material-ui/lab'
-
 import { createSmartFC, createStyles, IMyTheme } from 'src/common'
 import IconFilter from 'src/components/icons/IconFilter'
-import MyLink from 'src/components/MyLink'
 import Search from 'src/components/Search'
 import DefaultLayout from 'src/layouts/DefaultLayout'
 
@@ -52,12 +48,6 @@ export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes
             asideTitle='Filters'
             defaultOpen
         >
-            <Snackbar open={open} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
-                <Alert severity='warning' variant='filled' className={classes.alert} onClose={() => setOpen(false)}>
-                    Results don't include changes and new blueprints after Oct 22. Loading time may take up to 1 minute.
-                    <MyLink href='/blog/looking-for-new-host'>Learn more.</MyLink>
-                </Alert>
-            </Snackbar>
             <Search className={classes.search} />
             <Cards />
             <LoadMore />
