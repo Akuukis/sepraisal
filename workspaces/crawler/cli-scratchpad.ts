a4d5a03cc60c9220d300024e0710800e780c15c6
Akuukis <akuukis@kalvis.lv>
2021-04-30 16:17:04 +0300

    fix(crawler): scrachpad as improvised test

--- workspaces/crawler/cli-scratchpad.ts
+++ workspaces/crawler/cli-scratchpad.ts
@@ -5,11 +5,19 @@ import { join } from 'path'
 ;
 (async () => {
     const sePraisal = await NewPraisalManager()()
-
-    const praisal = await sePraisal.praiseSbc(readFileSync(join(VENDOR_DIR, 'prefabs', 'AtmosphericLander.sbc'), 'utf-8'))
-    // const praisal = await sepraisal.praiseSbc(readFileSync(join(__dirname, 'assets', 'blueprints', 'dekartaTests.sbc'), 'utf-8'))
+    const files = [
+        readFileSync(join(VENDOR_DIR, 'prefabs', 'AtmosphericLander.sbc'), 'utf-8'),
+        readFileSync(join(VENDOR_DIR, 'prefabs', 'AlienLander.sbc'), 'utf-8'),
+        readFileSync(join(VENDOR_DIR, 'prefabs', 'MarsLander.sbc'), 'utf-8'),
+        readFileSync(join(VENDOR_DIR, 'prefabs', 'RespawnShip.sbc'), 'utf-8'),
+        readFileSync(join(VENDOR_DIR, 'myBlueprints', 'dekartaTests.sbc'), 'utf-8'),
+        readFileSync(join(VENDOR_DIR, 'myBlueprints', 'EveryLargeBlockOnce.sbc'), 'utf-8'),
+    ]
+    console.time()
+    await Promise.all(files.map((file) => sePraisal.praiseSbc(file)))
+    console.timeEnd()
     // console.log(praisal.blummary.count)
     // console.log([...praisal.groups.entries()].map(([key, group]) => `${key}: ${group.blockCount}`))
-    console.log(praisal.integrityPlanes.maxValue)
+    // console.log(praisal.integrityPlanes.maxValue)
     // console.log(praisal.silhouette)
 })().catch((err) => console.error(err))
