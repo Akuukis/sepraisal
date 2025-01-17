d747c362559ea12b690d89dd4d325ce9d799b802
Snowboundmage2 <snowboundmage2@gmail.com>
2025-01-18 07:03:17 +1000

    updated snaps and added script for manual crawler

---
+++ workspaces/crawler/run_routines.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run routine 0
+echo "Running routine 0..."
+timeout 5h yarn launch 0 >> out-0.log 2>> err-0.log < /dev/null
+
+# Run routine 1
+echo "Running routine 1..."
+timeout 5h yarn launch 1 >> out-1.log 2>> err-1.log < /dev/null
+
+# Run routine 2
+echo "Running routine 2..."
+timeout 5h yarn launch 2 >> out-2.log 2>> err-2.log < /dev/null
+
+# Run routine 3
+echo "Running routine 3..."
+timeout 5h yarn launch 3 >> out-3.log 2>> err-3.log < /dev/null
+
+# Run routine 4
+echo "Running routine 4..."
+timeout 5h yarn launch 4 >> out-4.log 2>> err-4.log < /dev/null
+
+# Run routine 5
+#temp commenting due to error
+##echo "Running routine 5..."
+##timeout 4h yarn launch 5 >> out-5.log 2>> err-5.log < /dev/null
+
+# Run routine 5 in serial mode
+echo "Running routine 5 in serial mode..."
+timeout 1h yarn launch 5 --serial >> out-5.log 2>> err-5.log < /dev/null
+
+echo "All routines completed!"
