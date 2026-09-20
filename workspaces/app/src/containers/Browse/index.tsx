3c7490c6214ecaae4b47da4ba9ce3040df047d45
sarren <sarren@skygallant.com>
2026-09-20 08:19:23 +0000

    Restore browse UI with new public API

--- workspaces/app/src/containers/Browse/index.tsx
+++ workspaces/app/src/containers/Browse/index.tsx
@@ -1,10 +1,7 @@
-import { Container, Typography } from '@material-ui/core'
-import { Alert, AlertTitle } from '@material-ui/lab'
 import * as React from 'react'
 import { hot } from 'react-hot-loader/root'
 
 import { createSmartFC, createStyles, IMyTheme } from 'src/common'
-import AlertUkraine from 'src/components/AlertUkraine'
 import IconFilter from 'src/components/icons/IconFilter'
 import Search from 'src/components/Search'
 import DefaultLayout from 'src/layouts/DefaultLayout'
@@ -49,10 +46,9 @@ export default hot(createSmartFC(styles, __filename)<IProps>(({children, classes
             asideTitle='Filters'
             defaultOpen
         >
-            <AlertUkraine />
-            {/* <Search className={classes.search} enableSort enableStatus />
+            <Search className={classes.search} enableSort enableStatus />
             <Cards />
-            <LoadMore /> */}
+            <LoadMore />
         </DefaultLayout>
     )
 })) /* ============================================================================================================= */
